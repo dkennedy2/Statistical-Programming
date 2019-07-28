ff1649fc6567361a7e082e4dec63d4179a818290
David Kennedy <39226432+dkennedy2@users.noreply.github.com>
2019-07-28 17:34:22 -0500

    Add files via upload

--- assignment 2.py
+++ assignment 2.py
@@ -1,90 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 20 14:05:14 2019
+Created on Sat Jul 27 17:10:39 2019
 
 @author: JoAnne
 """
 
 print('CPSC-51100, [Summer] [2019]')
-print('NAME: David Kennedy')
+print('NAME: David Kennedy and Nisha George')
 print('PROGRAMMING ASSIGNMENT #2')
 
-
-fh = open('C:/Users/JoAnne/Desktop/hi.url','r')
-
-lineList = [line.rstrip('\n') for line in open('C:/Users/JoAnne/Desktop/hi.txt')]
+# Import file and put contents into a list.
+lineList = [line.rstrip('\n') for line in open('C:/Users/JoAnne/Desktop/prog2-input-data.txt')]
 floatList = [float(x) for x in lineList]
    
-#k = input("Enter the number of clusters: ")
-#k = int(k)
-k = 2 
-'''delete'''
+# Ask user for amount of clusters, assign number of clusters to k.
+k = input("Enter the number of clusters: ")
+k = int(k)
 
+# Initialize variables to store points and update centroids.
 centroids = dict(zip(range(k), floatList[0:k]))
 clusters = dict(zip(range(k), [[] for i in range(k)]))
-maping = dict(zip(range(10), floatList[0:10]))
+mapping = dict(zip(range(k), [[] for i in range(k)]))
 
-#clusters.update(0 = floatList[0])
-
-temp = 0
-minimum2 = 100
-differences = []
-#for x in range(k):
-#    for i in range(0,9):
-#            temp = abs(floatList[x] - floatList[i])
-#            
-#            if temp != 0:
-#                differences[x].append(temp)
-            
-#            if temp != 0:
-#                minimum = temp        
-#                if minimum2 < minimum:
-#                    minimum = minimum2
-#                else:
-#                    minimum = minimum
-
+c = 0
+temp2 = 0
+while temp2 != centroids:  # Program runs until centroids converge.
+    c = c + 1
+    temp2 = centroids.copy()
+    print(' ')
+    print('Iteration ' + str(c))
+    
+    # Clears clusters for next run.
+    clusters = dict(zip(range(k), [[] for i in range(k)]))
+    
+    # Find differences between points and centroid, stores in mapping dictionary.
+    for i in range(0, len(floatList)):
+        for ii in range(0, k):
+            mapping[ii] = (floatList[i], ii, abs(floatList[i] - centroids[ii]))
         
-for i in range(0,k): # set 5 to k - number of clusters
-    clusters[i].append(floatList[i])   
-    for ii in range(k,len(floatList)):
-        temp = abs(floatList[i] - floatList[ii])
-        if temp != 0:
-            differences.append((ii,temp))
-            differences.sort(key = lambda x: x[1])
-            
-            
-    clusters[i].append(floatList[differences[0][0]])
-    floatList[differences[0][0]] = 1000
-    floatList.remove(1000)
-    differences = []
+        # Find and add point with smallest difference to the cluster.
+        temp = (10, (100, 100, 100))
+        for key, value in dict(mapping).items():
+            aa = (key, value)
+            if temp[1][2] < aa[1][2]:
+                aa = temp
+            temp = aa
+        clusters[aa[1][1]].append(aa[1][0])
     
-    mean = sum(clusters[i])/len(clusters[i])
-    centroids[i] = mean
-
-#for i in range(0,k):
-#    mean = sum(clusters[i])/len(clusters[i])
-#    centroids[i] = mean
-            
-#    minimum = min(differences[1])
-#    clusters[0].append(10)
-
-
-
-
-
-
-
-
-
+    # Update centroids and print output.
+    for i in range(0, k):
+        centroids[i] = sum(clusters[i])/len(clusters[i])
+        print(str(i) + ' ' + str(clusters[i]))
     
-##for x in floatList:
-#for i in range(0,9):
-#    temp = abs(floatList[0] - floatList[i])
-#    if temp != 0:
-#        differences.append((i,temp))
-#        differences.sort(key = lambda x: x[1])
-#        
-#clusters[0].append(floatList[differences[0][0]])
-#        
-#minimum = min(differences[1])
-#clusters[0].append(10)
+print(' ') 
+text = [] 
+for point in lineList:
+    for key, value in clusters.items():
+        for x in value:
+            if point == str(x):
+                print('Point ' + str(point) + ' in cluster ' + str(key))
+                text.append('Point ' + str(point) + ' in cluster ' + str(key))
+                
+# Output file.                
+f = open('prog2-output-data.txt', 'w')
+for sentence in text:
+    f.write('\n' + str(sentence))
